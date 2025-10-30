2bc54c31e607fa6e83d8b558a304cfc7e86a3495
Hussnain <hussnainpucit@gmail.com>
2025-10-30 13:47:19 +0500

    Upgrades to packages

--- example/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Demo from './demo'
-import { createRoot } from 'react-dom/client'
-
-const container = document.getElementById('root')
-const root = createRoot(container!) // createRoot(container!) if you use TypeScript
-root.render(<Demo />)
